83033c67d291fdae5e442809a7ff7a88ba1c290f
madvaies <m.andrew.davies@gmail.com>
2024-12-31 21:20:22 +0000

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -10,7 +11,28 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Inherit from Next's recommended configs and TS config:
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Now specify your custom overrides:
+  {
+    rules: {
+      // Turn off the rule that complains about @ts-ignore, @ts-nocheck, etc.
+      "@typescript-eslint/ban-ts-comment": "off",
+
+      // Turn off the rule complaining about `any`
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // Turn off the rule complaining about <img> usage
+      "@next/next/no-img-element": "off",
+
+      // Turn off complaining about unescaped quotes/apostrophes
+      "react/no-unescaped-entities": "off",
+
+      // If you want to suppress warnings about using <Link> from 'next/link' vs anchor:
+      // "@next/next/no-html-link-for-pages": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
