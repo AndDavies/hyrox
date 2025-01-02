9f340e89b76388a204eef5cb17dd78632d79f616
madvaies <m.andrew.davies@gmail.com>
2025-01-02 05:33:28 +0000

    2 Jan - Lets Go

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores all linting errors during builds
+  },
+  /* Add other config options here if needed */
 };
 
 export default nextConfig;
